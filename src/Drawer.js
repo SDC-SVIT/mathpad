92545a2e7eba3f316bbf1fc92bfd3e4c654b0198
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2021-01-01 22:01:09 +0530

    Delta added.

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
 import EditorFile from "./EditorFile";
 import CustomListItem from "./CustomListItem";
@@ -15,241 +15,247 @@ import CustomListItem from "./CustomListItem";
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
     },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9) + 1,
-        },
-    },
-    toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(1),
-    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+  },
 }));
 
 export default function MiniDrawer() {
+  const sn = [
+    {
+      name: "Belongs To",
+      symbol: "∈",
+    },
+    {
+      name: "Null",
+      symbol: "∅",
+    },
+    {
+      name: "For All",
+      symbol: "∀",
+    },
+    {
+      name: "There Exists",
+      symbol: "∃",
+    },
+    {
+      name: "Delta",
+      symbol: "δ",
+    },
+    {
+      name: "Conjunction",
+      symbol: "∧",
+    },
+    {
+      name: "Disjunction",
+      symbol: "∨",
+    },
+    {
+      name: "Negation",
+      symbol: "¬",
+    },
+    {
+      name: "Conditional",
+      symbol: "→",
+    },
+    {
+      name: "Biconditional",
+      symbol: "↔",
+    },
+    {
+      name: "Implication",
+      symbol: "⇒",
+    },
+    {
+      name: "Equivalence",
+      symbol: "⇔",
+    },
 
-    const sn = [
-        {
-            "name": "Belongs To",
-            "symbol": "∈"
-        },
-        {
-            "name": "Null",
-            "symbol": "∅"
-        },
-        {
-            "name": "For All",
-            "symbol": "∀"
-        },
-        {
-            "name": "There Exists",
-            "symbol": "∃"
-        },
-        {
-            "name": "Conjunction",
-            "symbol": "∧"
-        },
-        {
-            "name": "Disjunction",
-            "symbol": "∨"
-        },
-        {
-            "name": "Negation",
-            "symbol": "¬"
-        },
-        {
-            "name": "Conditional",
-            "symbol": "→"
-        },
-        {
-            "name": "Biconditional",
-            "symbol": "↔"
-        },
-        {
-            "name": "Implication",
-            "symbol": "⇒"
-        },
-        {
-            "name": "Equivalence",
-            "symbol": "⇔"
-        },
-        
-        {
-            "name": "Less then, Equal To",
-            "symbol": "≤"
-        },
-        {
-            "name": "Square Root",
-            "symbol": "√"
-        },
-       
-        {
-            "name": "Subset",
-            "symbol": "⊆"
-        },
-       
-        {
-            "name": "! Belongs To",
-            "symbol": "∉"
-        },
-        {
-            "name": "! Subset of",
-            "symbol": "⊄"
-        },
-        {
-            "name": "Union",
-            "symbol": "∪"
-        },
-        {
-            "name": "Intersection",
-            "symbol": "∩"
-        },
-        {
-            "name": "! Equals To",
-            "symbol": "≠"
-        },
-        {
-            "name": "Sigma",
-            "symbol": "∑"
-        },
-        {
-            "name": "Mu",
-            "symbol": "μ"
-        },
-        {
-            "name": "Alpha",
-            "symbol": "α"
-        },
-        {
-            "name": "Beta",
-            "symbol":"β"
-        },
-        {
-            "name":"Theta",
-            "symbol":"θ"
-        },
-        {
-            "name":"Lambda",
-            "symbol":"λ"
-        },
-        {
-            "name":"Pi",
-            "symbol":"π"
-        },
-        {
-            "name":"Alef",
-            "symbol":"ℵ"
-        }
-    ];
-    
+    {
+      name: "Less then, Equal To",
+      symbol: "≤",
+    },
+    {
+      name: "Square Root",
+      symbol: "√",
+    },
 
+    {
+      name: "Subset",
+      symbol: "⊆",
+    },
 
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-    const editorRef = React.useRef(null);
+    {
+      name: "! Belongs To",
+      symbol: "∉",
+    },
+    {
+      name: "! Subset of",
+      symbol: "⊄",
+    },
+    {
+      name: "Union",
+      symbol: "∪",
+    },
+    {
+      name: "Intersection",
+      symbol: "∩",
+    },
+    {
+      name: "! Equals To",
+      symbol: "≠",
+    },
+    {
+      name: "Sigma",
+      symbol: "∑",
+    },
+    {
+      name: "Mu",
+      symbol: "μ",
+    },
+    {
+      name: "Alpha",
+      symbol: "α",
+    },
+    {
+      name: "Beta",
+      symbol: "β",
+    },
+    {
+      name: "Theta",
+      symbol: "θ",
+    },
+    {
+      name: "Lambda",
+      symbol: "λ",
+    },
+    {
+      name: "Pi",
+      symbol: "π",
+    },
+    {
+      name: "Alef",
+      symbol: "ℵ",
+    },
+  ];
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerToggle = ()=>{
-        setOpen(!open);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const editorRef = React.useRef(null);
 
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <Drawer
-                variant="permanent"
-                className={clsx(classes.drawer, {
-                    [classes.drawerOpen]: open,
-                    [classes.drawerClose]: !open,
-                })}
-                classes={{
-                    paper: clsx({
-                        [classes.drawerOpen]: open,
-                        [classes.drawerClose]: !open,
-                    }),
-                }}
-            >
-                <div className={classes.toolbar}>
-                    <IconButton onClick={handleDrawerToggle}>
-                        {!open? <MenuIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>
-                    {sn.map((text, index) => (
-                       <CustomListItem name={text.name} symbol={text.symbol} open={open} editor={editorRef}/>
-                    ))}
-                </List>
-                {/*<Divider />*/}
-                {/*<List>*/}
-                {/*    {['All mail', 'Trash', 'Spam'].map((text, index) => (*/}
-                {/*        <ListItem button key={text}>*/}
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-                {/*            <ListItemText primary={text} />*/}
-                {/*        </ListItem>*/}
-                {/*    ))}*/}
-                {/*</List>*/}
-            </Drawer>
-            <main className={classes.content}>
-                {/*<div className={classes.toolbar} />*/}
-                <EditorFile ref={editorRef}/>
-            </main>
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerToggle}>
+            {!open ? <MenuIcon /> : <ChevronLeftIcon />}
+          </IconButton>
         </div>
-    );
+        <Divider />
+        <List>
+          {sn.map((text, index) => (
+            <CustomListItem
+              name={text.name}
+              symbol={text.symbol}
+              open={open}
+              editor={editorRef}
+            />
+          ))}
+        </List>
+        {/*<Divider />*/}
+        {/*<List>*/}
+        {/*    {['All mail', 'Trash', 'Spam'].map((text, index) => (*/}
+        {/*        <ListItem button key={text}>*/}
+
+        {/*            <ListItemText primary={text} />*/}
+        {/*        </ListItem>*/}
+        {/*    ))}*/}
+        {/*</List>*/}
+      </Drawer>
+      <main className={classes.content}>
+        {/*<div className={classes.toolbar} />*/}
+        <EditorFile ref={editorRef} />
+      </main>
+    </div>
+  );
 }
