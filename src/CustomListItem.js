d8fb80cbc21ac8b7ba8705e4d257c8f625f903b6
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2021-01-02 11:28:35 +0530

    Added tooltips

--- src/CustomListItem.js
+++ src/CustomListItem.js
@@ -2,27 +2,35 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import copy from 'copy-to-clipboard'
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import { shortCuts } from './config';
+import { Tooltip } from "@material-ui/core";
 
 function CustomListItem(props) {
     const useStyles = makeStyles({
         listItemText: {
-          fontSize: '1.5rem'
-      }
+            fontSize: '1.5rem'
+        }
     });
-    
+    var title = shortCuts.filter(v =>
+        v.symbol === props.symbol
+    )[0].shortcut;
+    console.log(title);
+
     const classes = useStyles();
     const customCopy = () => {
         copy(props.symbol);
         props.editor.current.sendTextToEditor(props.symbol);
     }
     return (
-        <ListItem button key={props.name} onClick={customCopy}>
-            {props.open ? <ListItemText classes={{ primary: classes.listItemText }} primary={props.symbol}  secondary={props.name} /> 
-            : 
-            <ListItemText classes={{ primary: classes.listItemText }} primary={props.symbol} />}
+        <Tooltip title={title}>
+            <ListItem button key={props.name} onClick={customCopy}>
+                {props.open ? <ListItemText classes={{ primary: classes.listItemText }} primary={props.symbol} secondary={props.name} />
+                    :
+                    <ListItemText classes={{ primary: classes.listItemText }} primary={props.symbol} />}
 
-        </ListItem>
+            </ListItem>
+        </Tooltip>
     );
 }
 
