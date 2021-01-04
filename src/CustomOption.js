f80ffa3da93b779e16c79b9abd4b224c21679def
Ayaan Retiwala <ayaan.180410107098@gmail.com>
2021-01-04 14:21:10 +0530

    Slider added

--- src/CustomOption.js
+++ src/CustomOption.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import Typography from '@material-ui/core/Typography';
 import { blue } from '@material-ui/core/colors';
 import CanvasDraw from "react-canvas-draw";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import InputSlider from './InputSlider';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Input from '@material-ui/core/Input';
+import BrushIcon from '@material-ui/icons/Brush';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const PrettoSlider = withStyles({
+  root: {
+    color: '#52af77',
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+const useStyles = makeStyles((theme) => ({
   avatar: {
     backgroundColor: blue[100],
     color: blue[600],
@@ -23,14 +57,29 @@ const useStyles = makeStyles({
     height: '28px',
     border: '1px solid #F1F1F1',
     borderRadius: '2px',
-    background: 'white'
-  }
-});
+    background: 'white',
+
+  },
+  root: {
+    width: 300 + theme.spacing(3) * 2,
+    marginLeft: 'auto',
+    // marginRight: 'auto',
+  },
+  // margin: {
+  //   height: theme.spacing(3),
+  // },
+}));
+
+
+// const useStyles = makeStyles({
+
+// });
 
 function Canvas(props) {
   const classes = useStyles();
   const saveableCanvas = React.useRef(null);
   const { onClose, selectedValue, open } = props;
+  const [value, setValue] = React.useState(4);
 
   const handleClose = () => {
     onClose(selectedValue);
@@ -43,6 +92,23 @@ function Canvas(props) {
     );
   }
 
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(Number(newValue));
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? 0 : Math.min(Number(event.target.value), 30));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 30) {
+      setValue(30);
+    }
+  };
+
   return (
     <Dialog fullScreen onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
       <AppBar className={classes.appBar}>
@@ -53,6 +119,39 @@ function Canvas(props) {
           <Typography variant="h6" className={classes.title}>
             Canvas
             </Typography>
+          <div className={classes.root}>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item>
+                <BrushIcon />
+              </Grid>
+              <Grid item xs>
+                <PrettoSlider
+                  value={typeof value === 'number' ? value : 0}
+                  onChange={handleSliderChange}
+                  aria-labelledby="input-slider"
+                  step={2}
+                  min={1}
+                  max={30}
+                />
+              </Grid>
+              <Grid item>
+                <Input
+                  className={classes.input}
+                  value={value}
+                  margin="dense"
+                  onChange={handleInputChange}
+                  onBlur={handleBlur}
+                  inputProps={{
+                    step: 2,
+                    min: 1,
+                    max: 30,
+                    type: 'number',
+                    'aria-labelledby': 'input-slider',
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </div>
           <div className={classes.toolbarButtons}>
             <Button color="inherit" onClick={() => { saveableCanvas.current.clear(); saveCanvas(); }}>
               Clear
@@ -61,13 +160,15 @@ function Canvas(props) {
               Undo
             </Button>
           </div>
+
         </Toolbar>
       </AppBar>
       <CanvasDraw
         ref={saveableCanvas}
         saveData={localStorage.getItem("savedDrawing")}
-        brushRadius={4}
+        brushRadius={value}
         onChange={saveCanvas}
+        lazyRadius={0}
         immediateLoading={true}
         style={{
           width: '100%',
---
+++ src/InputSlider.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import Input from '@material-ui/core/Input';
+import BrushIcon from '@material-ui/icons/Brush';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+
+const PrettoSlider = withStyles({
+    root: {
+      color: '#52af77',
+      height: 8,
+    },
+    thumb: {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      marginTop: -8,
+      marginLeft: -12,
+      '&:focus, &:hover, &$active': {
+        boxShadow: 'inherit',
+      },
+    },
+    active: {},
+    valueLabel: {
+      left: 'calc(-50% + 4px)',
+    },
+    track: {
+      height: 8,
+      borderRadius: 4,
+    },
+    rail: {
+      height: 8,
+      borderRadius: 4,
+    },
+  })(Slider);
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      width: 300 + theme.spacing(3) * 2,
+    },
+    margin: {
+      height: theme.spacing(3),
+    },
+  }));
+
+export default function InputSlider() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(30);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <BrushIcon />
+        </Grid>
+        <Grid item xs>
+          <PrettoSlider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            className={classes.input}
+            value={value}
+            margin="dense"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
